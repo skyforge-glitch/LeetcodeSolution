1dacb0f4dc4de8165015096f91999348ac15ddcc
Akash Singh <akashsingh.coding@gmail.com>
2025-11-12 19:31:14 +0530

    Added 455 Solution

---
+++ 455_AssignCookies/455_AssignCookies.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int m = g.size(), n = s.size();
+        int i = 0, j = 0;
+        while(i < m && j < n){
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+        return i;
+    }
+};
